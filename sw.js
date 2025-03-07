6bdeb094881269458b981be6b848a4988167d204
Douglas S. <dougssantana@gmail.com>
2025-03-07 16:37:02 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/myapp.html",
+        ]),
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request));
+});
